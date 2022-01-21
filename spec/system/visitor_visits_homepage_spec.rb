283a766da32ccfa4f7c4b3989028b15ad4c91fa4
Hikari-desuyoo <hikaridesuyoo@gmail.com>
2022-01-21 11:12:31 -0300

    automatizacao das networks no docker(integracao ainda nao funciona)

--- spec/system/visitor_visits_homepage_spec.rb
+++ spec/system/visitor_visits_homepage_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe 'Visitor visit homepage' do
   it 'successfully' do
+    fake_response(
+      status: 200, 
+      json: 'user_stats_200.json', 
+      route: '/api/v1/users_stats', 
+      method: :get
+    )
     visit root_path
     expect(page).to have_content(I18n.t('home.index.welcome'))
   end
